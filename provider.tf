e18076f3adea4b7387342fe51af8b0e9701124fb
PK <pkang81@gmail.com>
2021-02-17 06:48:40 -0500

    Initial

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
+    region = var.AWS_REGION
+}
+
+data "aws_availability_zones" "available" {
+}
+
+provider "http" {
+}
